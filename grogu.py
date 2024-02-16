af73693b10b488445b74ba8735122d56d31a8be8
HesaraMahela <hesaramahela@outlook.com>
2024-02-16 08:16:05 +0530

    new version

--- grogu.py
+++ grogu.py
@@ -1,8 +1,12 @@
 import sys
+
+from LogReader.log_file_manager import LogFileManager
+from calculations.calculation_manager import run_calculation
 from calculations.gravitypoint import gravity_point
 from inputFileGeneration.coordinates import coordinate_generation,string_of_atoms_coordinates
 from atoms.atoms import Atom
 from inputFileGeneration.input_file_writer import input_file_config
+from LogReader.log_file_reader import get_input_files_list,find_corresponding_output_file
 
 file_path = sys.argv[1]
 
@@ -10,6 +14,7 @@ variable_parameters =[]
 file_data=""
 atom_list = []
 # todo: charge and multiplicity from the input file
+
 try:
     with open(file_path,'r') as file:
         file_data=file.read()
@@ -31,4 +36,26 @@ coordinates=coordinate_generation(atom_list,step_count,step_size)
 for number, coordinate in enumerate(coordinates):
     coordinate_string = string_of_atoms_coordinates(atom_list,coordinate)
     input_file_config(number,coordinate_string)
+
+
+
+input_files = get_input_files_list()
+
+output_file_list =[]
+
+for file in input_files:
+    run_calculation(file)
+    print(find_corresponding_output_file(file), file)
+    log = LogFileManager(find_corresponding_output_file(file))
+    output_file_list.append(log)
+
+
+for obj in output_file_list:
+    print(obj.scf_done)
+
+
+
+
+
+
 

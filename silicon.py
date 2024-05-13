83860eadb8fe627fd4166f57314d1190d1fb5471
HesaraMahela <hesaramahela@outlook.com>
2024-05-13 12:33:16 +0530

    function was added to send previous calculations to archive

--- silicon.py
+++ silicon.py
@@ -1,6 +1,9 @@
+from matplotlib import pyplot as plt
+
 from LogReader.log_file_manager import LogFileManager
 from LogReader.log_file_reader import get_input_files_list, find_corresponding_output_file
 from atoms.atoms import Atom
+from settings import input_file_directory
 from calculations.Is_too_close import is_not_highly_repulsive
 from calculations.calculation_manager import run_calculation
 from calculations.random_spherical_coords_generator import random_spherical_coordinates_generator
@@ -14,30 +17,37 @@ from system import System
 create silicon atoms in the coordination sphere of atoms
 """
 
-number_of_silicon_atoms = 3
-radius = 10
 
-silicon_atoms = []
-for n in range(number_of_silicon_atoms):
-    silicon_atoms.append(Atom("Si", *random_spherical_coordinates_generator(10)))
-
-silicon_cluster = Molecule(silicon_atoms)
-coordinates = spherical_gird_coordinate_generation(silicon_cluster, 7, 1)
-
-system = System(silicon_atoms, 0, 1)
-
-for number, coordinate in enumerate(coordinates):
-    coordinate_string = string_of_atoms_coordinates(silicon_cluster.atoms, coordinate)
-    print(coordinate_string)
-    input_file_number = number
-    input_file_config(input_file_number, coordinate_string, system)
+def plot_the_graph(outputFiles, file_name="output.jpg"):
+    data = [float(f.scf_done[0]) for f in outputFiles if f.scf_done != "could not found"][:-1]
+    plt.plot(data)
+    plt.ylabel("Energy/AU")
+    plt.xlabel("Step")
+    plt.savefig(input_file_directory + "/" + file_name)
 
 
-####################################################################################
-all_input_files = get_input_files_list()
-output_file_list = []
+def run(num_of_silicon_atoms=3, radius=10):
+    silicon_atoms = []
+    for n in range(num_of_silicon_atoms):
+        silicon_atoms.append(Atom("Si", *random_spherical_coordinates_generator(radius)))
 
-for file in all_input_files:
+    silicon_cluster = Molecule(silicon_atoms)
+    coordinates = spherical_gird_coordinate_generation(silicon_cluster, 7, 1)
+
+    system = System(silicon_atoms, 0, 1)
+
+    for number, coordinate in enumerate(coordinates):
+        coordinate_string = string_of_atoms_coordinates(silicon_cluster.atoms, coordinate)
+        print(coordinate_string)
+        input_file_number = number
+        input_file_config(input_file_number, coordinate_string, system)
+
+    ####################################################################################
+
+    all_input_files = get_input_files_list()
+    output_file_list = []
+
+    for file in all_input_files:
 
         if run_calculation(file) != 0:  # something went wrong  thus no log file is produced
             continue
@@ -45,19 +55,11 @@ for file in all_input_files:
         log = LogFileManager(find_corresponding_output_file(file))
         output_file_list.append(log)
 
-
-for obj in output_file_list:
-    print(obj.scf_done)
-
-import matplotlib.pyplot as plt
+    for obj in output_file_list:
+        print(obj.scf_done)
+    plot_the_graph(output_file_list)
 
 
-def plot_the_graph(outputFiles, file_name="output.jpg"):
-    data = [float(f.scf_done[0]) for f in outputFiles if f.scf_done != "could not found"][:-1]
-    plt.plot(data)
-    plt.ylabel("Energy/AU")
-    plt.xlabel("Step")
-    plt.savefig(file_name)
 
 
-plot_the_graph(output_file_list)
+run()

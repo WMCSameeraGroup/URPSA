4b999ae52658125ce143e50bc142f3816a8874a1
HesaraMahela <hesaramahela@outlook.com>
2024-05-17 20:25:40 +0530

    number of cores implementation error was fixed

--- inputFileGeneration/input_template.py
+++ inputFileGeneration/input_template.py
@@ -2,7 +2,7 @@ from settings import input_file_directory as inp_dir
 from settings import method,number_of_cores
 
 
-def get_input_template(number, system, calc_method=method):
+def get_input_template(number, system, calc_method=method,num_of_cores=number_of_cores):
     # gonna replace title value
     return """%NProcShared={5}
 %chk={3}/test{0}.chk
@@ -10,7 +10,7 @@ def get_input_template(number, system, calc_method=method):
 
 Title{0}
  
-{1} {2}	\n""".format(number, system.charge, system.multiplicity, inp_dir, calc_method,number_of_cores)
+{1} {2}	\n""".format(number, system.charge, system.multiplicity, inp_dir, calc_method,num_of_cores)
 
 
 if __name__ == "__main__":

165f87cd9f34d5efd6777f26c3dc39e54e9c3cf9
HesaraMahela <hesaramahela@outlook.com>
2025-01-03 12:54:09 +0530

    redundant code was removed

--- calculations/Is_too_close.py
+++ calculations/Is_too_close.py
@@ -2,44 +2,24 @@ from atoms.atoms import Atom
 
 
 
-def make_atoms_from_input_file(file, n_origin_atoms,input_file_directory):
-    data = read_com(file,input_file_directory).split("\n\n")
-    atoms_strings = data[1].split('\n')[3:]
-    atoms = []
-    for atom_string in atoms_strings:
-        atoms.append(Atom(*atom_string.split()))
-    return atoms[0:n_origin_atoms], atoms[n_origin_atoms:]
-
-
-def read_com(file,input_file_directory):
-    if "/" in file:  # if full path is given
-        with open(file, 'r') as com:
-            return com.read()
-    else:
-        with open(input_file_directory() + '/' + file, 'r') as com:
-            return com.read()
-
-
-def is_too_close(origin_atoms, moving_atoms, stop_distance_factor=0.5):
-    for i in origin_atoms:
-        for j in moving_atoms:
-
-            if i.distance_between(j) < (
-                    i.v_radius + j.v_radius) * stop_distance_factor:
-                print(i.distance_between(j), i, j)
-                return False
-    return True
-
-
-def is_not_highly_repulsive(file, n_origin_atoms):
-    moving_atoms, origin_atoms = make_atoms_from_input_file(file, n_origin_atoms)
-    return is_too_close(moving_atoms, origin_atoms)
-
 
 ################################### for spherical #####################################
 
 def is_too_close_spherical(atoms1, atoms2, stop_distance_fac):
-    """only atoms in 2 molecules are compered """
+    """
+    Checks if any two atoms from two different molecules are too close to each other.
+
+    This function compares the distances between all pairs of atoms from two different molecules.
+    If any pair of atoms is within a critical distance — determined by the sum of their van der Waals
+    radii scaled by `stop_distance_fac` — the function returns False, indicating that the atoms are
+    too close. Otherwise, it returns True.
+
+    :param atoms1: A list of `Atom` objects from the first molecule.
+    :param atoms2: A list of `Atom` objects from the second molecule.
+    :param stop_distance_fac: A scaling factor to adjust the threshold distance for considering atoms
+                              as too close (float).
+    :return: True if no atoms are too close; False if any atoms are within the critical distance.
+    """
     for i in atoms1:
         for j in atoms2:
             if i.distance_between(j) < (
@@ -54,7 +34,16 @@ def is_too_close_spherical(atoms1, atoms2, stop_distance_fac):
 
 def is_not_highly_repulsive_spherically(sys, stop_distance_fac=0.5):
     """
-    check the distance between atoms of different molecules
+    Checks if the atoms of different molecules in the system are not too close to each other.
+
+    This function iterates through all pairs of molecules in the system and verifies
+    that no two atoms from different molecules are within a highly repulsive distance.
+    The check is performed using a spherical distance criterion with a scaling factor.
+
+    :param sys: The system object containing a list of molecules.
+    :param stop_distance_fac: A scaling factor to adjust the threshold distance for repulsion (float).
+                              Default is 0.5.
+    :return: True if no atoms are within the highly repulsive distance; False otherwise.
     """
     for molecule_1 in sys.molecules:
         for molecule_2 in sys.molecules:

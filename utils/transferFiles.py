19fda054604abe2358b6c57549407c27eaac6245
HesaraMahela <hesaramahela@outlook.com>
2025-01-08 12:26:58 +0530

    removed redundent funtions and add one useful

--- utils/transferFiles.py
+++ utils/transferFiles.py
@@ -1,37 +1,6 @@
 import os
-import shutil
-from datetime import datetime
 
-def move_files_to_timestamped_folder(source_dir="inputFiles"):
-    # Generate timestamp for folder name
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    # Create new folder with timestamp
-    new_folder = os.path.join(os.getcwd(), "Archives", timestamp)
-    os.makedirs(new_folder)
-
-    # Get all files in the source directory
-    files = [f for f in os.listdir(source_dir) if os.path.isfile(os.path.join(source_dir, f))]
-
-    # Move each file to the new folder
-    for file in files:
-        shutil.move(os.path.join(source_dir, file), new_folder)
-
-    print(f"All files  moved to folder 'Archives/{timestamp}'.")
-
-
-
-def move_files_to_project_folder(dir_name, source_dir="inputFiles"):
-
-    # Create new folder with file name
-    new_folder = os.path.join(os.getcwd(), "Projects", dir_name)
-    os.makedirs(new_folder)
-
-    # Get all files in the source directory
-    files = [f for f in os.listdir(source_dir) if os.path.isfile(os.path.join(source_dir, f))]
-
-    # Move each file to the new folder
-    for file in files:
-        shutil.move(os.path.join(source_dir, file), new_folder)
-
-    print(f"All files  moved to folder.")
+def move_dir_to_archive(source_dir,destination_dir):
+    os.rename(source_dir, destination_dir)
+    print(f"Moved {source_dir} to {destination_dir}")
 

61a0e089f51fe1a0c87f26e707710e2279fc2b2f
HesaraMahela <hesaramahela@outlook.com>
2025-01-08 12:33:25 +0530

    add methods to remove and change path of dirs

--- utils/transferFiles.py
+++ utils/transferFiles.py
@@ -1,6 +1,28 @@
 import os
 
 def move_dir_to_archive(source_dir,destination_dir):
-    os.rename(source_dir, destination_dir)
-    print(f"Moved {source_dir} to {destination_dir}")
+    try:
+        os.rename(source_dir, destination_dir)
+        print(f"Moved {source_dir} to {destination_dir}")
+    except FileNotFoundError:
+        print("The source directory does not exist.")
+    except PermissionError:
+        print("Permission denied.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+
+def delete_dir(source_dir,destination_dir):
+    try:
+        os.rmdir(source_dir)
+        print(f"Removed {source_dir}")
+
+    except FileNotFoundError:
+        print("The source directory does not exist.")
+
+    except PermissionError:
+        print("Permission denied.")
+    except Exception as e:
+        print(f"Error: {e}")
 
